50b5cbed76bea7fc07bdfffbf972d23f935df2b7
Braden M. Kelley <redbmk@gmail.com>
2018-09-19 18:13:54 -0700

    add basic graphql server

---
+++ src/server/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const cors = require('cors');
+const graphqlHTTP = require('express-graphql');
+const gql = require('graphql-tag');
+const { buildASTSchema } = require('graphql');
+
+const POSTS = [
+  { author: "John Doe", body: "Hello world" },
+  { author: "Jane Doe", body: "Hi, planet!" },
+];
+
+const schema = buildASTSchema(gql`
+  type Query {
+    posts: [Post]
+    post(id: ID): Post
+  }
+
+  type Post {
+    id: ID
+    author: String
+    body: String
+  }
+`);
+
+const mapPost = (post, id) => post && ({ id, ...post });
+
+const root = {
+  posts: () => POSTS.map(mapPost),
+  post: ({ id }) => mapPost(POSTS[id], id),
+};
+
+const app = express();
+app.use(cors());
+app.use('/graphql', graphqlHTTP({
+  schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+const port = process.env.PORT || 4000
+app.listen(port);
+console.log(`Running a GraphQL API server at localhost:${port}/graphql`);
